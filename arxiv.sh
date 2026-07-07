13a00e815aa18282a87251e87c80166cf6adf224
Farid Aliyev <faridalieev@gmail.comm>
2026-07-07 15:41:45 +0400

    Ilk arxivleme skripti elave olundu

---
+++ arxiv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+    echo "XETA: Fayl adini yazmagi unutdun!"
+    exit 1
+fi
+if [ -f "$1" ]; then
+    cp "$1" "$1.bak"
+    echo "Ugurla bitdi! $1 faylinin arxivi yaradildi."
+else
+    echo "Brat, $1 adinda fayl yoxdur."
+fi
