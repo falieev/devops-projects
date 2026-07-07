b50b6b6ba25fcd9767b59531b68865f57b00d2df
Farid Aliyev <faridalieev@gmail.com>
2026-07-07 16:04:25 +0400

    Skripte tarix funksiyasi elave olundu

--- arxiv.sh
+++ arxiv.sh
@@ -5,7 +5,7 @@ if [ -z "$1" ]; then
 fi
 if [ -f "$1" ]; then
     cp "$1" "$1.bak"
-    echo "Ugurla bitdi! $1 faylinin arxivi yaradildi."
+    echo "Ugurla bitdi! $1 faylinin arxivi yaradildi. Tarix: $(date)"
 else
     echo "Brat, $1 adinda fayl yoxdur."
 fi
